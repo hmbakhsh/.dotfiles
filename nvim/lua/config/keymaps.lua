35f9103a46e1f0e241e4df54950adb324772e986
Haroon Bakhsh <h@hbak.co>
2025-11-15 13:34:38 +0000

    feat(nvim): add TypeScript error translator and improved diagnostics
    - Add ts-error-translator.nvim plugin for better TypeScript error messages
    - Enable inline diagnostics with virtual text
    - Add keymaps for better diagnostic navigation (<leader>dd/dl/dw)
    - Configure diagnostic display with severity sorting

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Better diagnostic navigation
+vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Show diagnostic under cursor" })
+vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics bufnr=0<cr>", { desc = "List buffer diagnostics" })
+vim.keymap.set("n", "<leader>dw", "<cmd>Telescope diagnostics<cr>", { desc = "List workspace diagnostics" })
--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -1,4 +1,12 @@
 return {
   -- Import LazyVim's TypeScript extras
   { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  -- TypeScript error translator
+  {
+    "dmmulroy/ts-error-translator.nvim",
+    config = function()
+      require("ts-error-translator").setup()
+    end,
+  },
 }
