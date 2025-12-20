4b3ff1842004dcc0fffc79d9417a8e0afe9c75f2
Haroon Bakhsh <h@hbak.co>
2025-12-20 08:49:15 +0000

    fix(nvim): disable spell checking in markdown files
    Spell checking was causing distracting wavy underlines in markdown files.
    Also enabled concealment settings to support markdown rendering plugins.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,11 +7,14 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Disable markdown diagnostics/warnings but keep formatting
+-- Disable markdown diagnostics/warnings and spell checking, enable concealment
 vim.api.nvim_create_autocmd({ "FileType", "BufEnter", "BufWinEnter" }, {
   pattern = { "markdown", "*.md" },
   callback = function(args)
     vim.diagnostic.enable(false, { bufnr = args.buf })
     vim.diagnostic.hide(nil, args.buf)
+    vim.opt_local.spell = false
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.concealcursor = ""
   end,
 })
